990a2eb09d86b771302c1026c34f5a263b4fbd9f
git-jessryll-workspace <jessryll.workspace@gmail.com>
2021-08-30 09:18:41 +0800

    create verification detail component

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import {
   AuthContext,
 } from "../context/state";
 import AuthLayout from "./AuthLayout";
 
 export default function Layout({ children }) {
-  const auth = useContext(AuthContext);
+ const auth = useContext(AuthContext);
   return (
     <>
     {
